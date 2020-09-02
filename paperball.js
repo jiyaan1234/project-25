5636cfad2043db146a0809dcd1fac0db1d928390
jiyaan1234 <67227958+jiyaan1234@users.noreply.github.com>
2020-09-02 17:18:55 +0530

    Add files via upload

---
+++ paperball.js
@@ -0,0 +1,27 @@
+class paperball {
+    constructor(x, y,radius) {
+      var options = {
+          isStatic : false ,
+          'restitution':0.3,
+          'friction':0.5,
+          'density':1.2
+      }
+      this.body = Bodies.circle(x, y, radius , options);
+      this.radius=radius;
+      this.image = loadImage("paper.png");
+      World.add(world, this.body);
+    }
+    display(){
+      
+      var angle = this.body.angle;
+      push();
+      translate(pos.x,pos.y);
+      rotate(angle)
+      imageMode(CENTER);
+        image(this.image, 0, 0, this.radius, this.radius);
+       pop();
+    }
+  };
+
+  
+  
